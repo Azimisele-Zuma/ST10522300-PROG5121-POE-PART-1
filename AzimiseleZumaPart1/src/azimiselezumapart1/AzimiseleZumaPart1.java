c90cc31730549536e65a526b3d03973cb9da2b36
Azimisele-Zuma <azimiselezuma@gmail.com>
2026-09-18 09:17:17 +0200

    Update AzimiseleZumaPart1.java

--- AzimiseleZumaPart1/src/azimiselezumapart1/AzimiseleZumaPart1.java
+++ AzimiseleZumaPart1/src/azimiselezumapart1/AzimiseleZumaPart1.java
@@ -2,6 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
+//package name
 package azimiselezumapart1;
 
 /**
